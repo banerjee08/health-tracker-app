232367d4cec6c576db84a80018d7fa35a610c5dd
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2024-02-04 02:53:28 +0530

    Store configured

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,19 @@
 // import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 
+import { configureStore } from "@reduxjs/toolkit";
+import { addActivityReducer } from "../redux/reducers/addActivityReducer";
+import { dailyActivityReducer } from "../redux/reducers/dailyActivityReducer";
+
 // export const store = configureStore({
 //   reducer: {
 //     counter: counterReducer,
 //   },
 // });
+
+export const store = configureStore({
+    reducer:{
+        addActivityReducer,
+        dailyActivityReducer
+    }
+});
